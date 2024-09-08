7271e157fb89ec1eb3e1156dccda1034370c64ce
Matthew <matthew.mcroberts22@gmail.com>
2024-09-08 13:32:56 -0400

    module implementation

--- minigame-plugin/build.gradle.kts
+++ minigame-plugin/build.gradle.kts
@@ -15,6 +15,8 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 
+    implementation(project(":minigame-common"))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- minigame-plugin/src/main/java/com/matthew/plugin/Minigame.java
+++ minigame-plugin/src/main/java/com/matthew/plugin/Minigame.java
@@ -4,10 +4,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Minigame extends JavaPlugin {
 
-
     @Override
     public void onLoad() {
-
     }
 
     @Override
